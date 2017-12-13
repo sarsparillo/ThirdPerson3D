cfecb3c432da0d649801c91e68927979c203aefb
Tom Whalley <whalleywrites@gmail.com>
2017-12-13 14:44:13 -0500

    Added some very basic actions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,40 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float jumpHeight;
 
+	private bool canJump, canMelee, canShoot;
+
 	public Rigidbody rb;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+
+		canJump = true;
 	}
 
 	
-	void Update () {
+	void Update() {
 		rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
+
+		if (Input.GetButtonDown("Jump")) {
+			Jump();
+		}
+	}
+
+	void Jump() {
+		if (canJump) {
+			rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+		}
+	}
+
+	void Melee() {
+		if (canMelee) {
+			Debug.Log("Melee key pressed.");
+		}
+	}
+
+	void Shoot() {
+		if (canShoot) {
+			Debug.Log("Shoot key pressed.");
+		}
 	}
 }
